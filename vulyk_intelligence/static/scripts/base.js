9e6b115af2333e4d47b72e706f5cee160656cf79
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-10-19 12:10:42 +0300

    More fields

--- vulyk_intelligence/static/scripts/base.js
+++ vulyk_intelligence/static/scripts/base.js
@@ -1,13 +1,14 @@
 $(function() {
-	var template = Handlebars.compile($('#intelligence_template').html()),
-		output = $("#out");
+    var template = Handlebars.compile($('#intelligence_template').html()),
+        output = $("#out");
 
     $(document.body).on("vulyk.next", function(e, data) {
-    	output.html(template(data.result.task.data));
-
+        output.html(template(data.result.task.data));       
     }).on("vulyk.save", function(e, callback) {
-    	callback($('textarea').serializeJSON());
+        callback($('textarea, input[type=checkbox]').serializeJSON());
+        $("html, body").animate({ scrollTop: 0 }, "slow");
     }).on("vulyk.skip", function(e, callback) {
         callback();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
     });
 });
